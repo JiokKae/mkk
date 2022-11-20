b5e8eb92faa9146d8e5380869af8aaca66565fae
JiokKae <ekaktm1@gmail.com>
2022-11-21 05:54:46 +0900

    Add ProfileCard component

---
+++ src/components/ProfileCard.js
@@ -0,0 +1,127 @@
+import { Col, Row } from "react-bootstrap";
+import styled, { css } from "styled-components";
+import { classLocale } from "../constants/lostark";
+import { MKK } from "../constants/urls";
+import UnregisterCharacterModal from "./UnregisterCharacterModal";
+
+const Card = styled.div`
+	position: relative;
+	border: 1px solid #cbcbcb;
+	padding: 0.5rem 1rem;
+	background: white;
+	border-radius: 1rem;
+	box-shadow: 0 0.125rem 0.25rem 0.0625rem #aaa;
+`;
+
+const NotRegistered = styled.div`
+	z-index: 9;
+	padding: 1.25rem 0;
+	position: absolute;
+	color: red;
+	background-color: #ffffffc9;
+	font-size: 1.25rem;
+	left: 50%;
+	top: 50%;
+	width: 100%;
+	transform: translate(-50%, -50%);
+	display: flex;
+	justify-content: center;
+`;
+
+const BoldDiv = styled.div`
+	font-weight: bold;
+`;
+
+const OrangeDiv = styled.div`
+	color: #f25400;
+	margin: auto;
+`;
+
+const ClassImg = styled.img`
+	width: 3.625rem;
+	height: 3.625rem;
+	${(props) =>
+		props.empty &&
+		css`
+			background-color: #aaa;
+			border-radius: 50%;
+		`};
+`;
+
+function ClassImage({ className }) {
+	return (
+		<>
+			{className === "" ? (
+				<ClassImg empty />
+			) : (
+				<ClassImg
+					src={`https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/${className}.png`}
+				/>
+			)}
+		</>
+	);
+}
+
+function ItemLevel({ itemLevel }) {
+	const [integer, decimal] = itemLevel.split(".");
+	return (
+		<>
+			{integer || "???"}.<small>{decimal || "??"}</small>
+		</>
+	);
+}
+
+export default function ProfileCard({ character, allow }) {
+	return (
+		<div className="col-6 col-md-4">
+			<Card>
+				{allow || <NotRegistered>등록 대기</NotRegistered>}
+				<Row className="gx-2 mb-2 flex-nowrap align-items-end">
+					<Col xs="auto">
+						<img
+							src={`${MKK}/img/guild.png`}
+							style={{
+								maxHeight: "1.5rem",
+							}}
+						/>
+					</Col>
+					<Col className="overflow-hidden">
+						<div
+							className="singleLine"
+							style={{
+								fontWeight: "bold",
+								color: "#006265",
+							}}>
+							{character.guild?.name || "???"}
+						</div>
+					</Col>
+					<Col xs="auto">
+						<UnregisterCharacterModal
+							characterName={character.name}
+						/>
+					</Col>
+				</Row>
+				<div className="row gx-1 mb-2">
+					<div className="col-auto">
+						<ClassImage className={character.class} />
+					</div>
+					<div className="col-auto" style={{ margin: "auto" }}>
+						<BoldDiv style={{ color: "#4761b4" }}>
+							{character.server || "???"}
+						</BoldDiv>
+						<span style={{ color: "black" }}>
+							Lv. {character.level || "??"}
+						</span>
+					</div>
+					<OrangeDiv className="col-auto">
+						<BoldDiv>{classLocale(character.class, "kr")}</BoldDiv>
+						<ItemLevel itemLevel={character.itemLevel} />
+					</OrangeDiv>
+				</div>
+				<div className="singleLine">
+					<span>{character.name}</span>
+				</div>
+			</Card>
+		</div>
+	);
+}
---
+++ src/constants/lostark.js
@@ -0,0 +1,41 @@
+export const CLASS_LOCALE = {
+	destroyer: { kr: "디스트로이어" },
+	berserker: { kr: "버서커" },
+	warlord: { kr: "워로드" },
+	holyknight: { kr: "홀리나이트" },
+	fighter: { kr: "무도가(여)" },
+	soulmaster: { kr: "기공사" },
+	force_master: { kr: "기공사" },
+	battlemaster: { kr: "배틀마스터" },
+	battle_master: { kr: "배틀마스터" },
+	striker: { kr: "스트라이커" },
+	infighter: { kr: "인파이터" },
+	lancemaster: { kr: "창술사" },
+	gunslinger: { kr: "건슬링어" },
+	devilhunter: { kr: "데빌헌터" },
+	devil_hunter: { kr: "데빌헌터" },
+	blaster: { kr: "블래스터" },
+	scouter: { kr: "스카우터" },
+	hawkeye: { kr: "호크아이" },
+	bard: { kr: "바드" },
+	summoner: { kr: "서머너" },
+	sorceress: { kr: "소서리스" },
+	elemental_master: { kr: "소서리스" },
+	arcana: { kr: "아르카나" },
+	demonic: { kr: "데모닉" },
+	reaper: { kr: "리퍼" },
+	blade: { kr: "블레이드" },
+	artist: { kr: "도화가" },
+	yinyangshi: { kr: "도화가" },
+	weather_artist: { kr: "기상술사" },
+};
+
+export const classLocale = (enClass, locale) => {
+	if (enClass === "") {
+		return "???";
+	}
+	if (CLASS_LOCALE.hasOwnProperty(enClass) === false) {
+		return enClass;
+	}
+	return CLASS_LOCALE[enClass][locale];
+};

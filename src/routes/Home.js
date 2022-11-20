a218af091bcf46e7c4fc376ceb9e5b1cc1a046ee
JiokKae <ekaktm1@gmail.com>
2022-11-21 06:00:37 +0900

    Add MyCharacters component to Home routes

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,18 @@
+import { useQuery } from "@apollo/client";
+import styled from "styled-components";
+import MyCharacters from "../components/MyCharacters";
+import { ME } from "../constants/querys";
+
+const Body = styled.div`
+	max-width: 780px;
+	margin: auto;
+`;
+
 export default function Home() {
-	return <div>MKK HOME</div>;
+	const { data } = useQuery(ME);
+	return (
+		<Body>
+			{data?.me ? <MyCharacters /> : null}
+		</Body>
+	);
 }

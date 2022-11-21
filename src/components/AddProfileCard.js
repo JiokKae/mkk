567be8d11803f62e71fc1b51c3017f575342ac69
JiokKae <ekaktm1@gmail.com>
2022-11-22 05:53:56 +0900

    Create AddProfileCard component

---
+++ src/components/AddProfileCard.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { MKK } from "../constants/urls";
+
+const Card = styled.div`
+	@media (max-width: 576px) {
+		min-height: 111.5px;
+	}
+	position: relative;
+	border: 1px solid #cbcbcb;
+	border-radius: 1rem;
+	padding: 0.5rem 1rem;
+	height: 100%;
+	min-height: 144.84px;
+	background-color: #a2a2a2;
+	box-shadow: inset 0 0.125rem 0.25rem 0.0625rem #666;
+`;
+
+export default function AddProfileCard({ onClick }) {
+	return (
+		<div className="col-6 col-md-4">
+			<Card
+				className="d-flex justify-content-center align-items-center"
+				onClick={onClick}>
+				<img src={`${MKK}/img/plus.png`} />
+			</Card>
+		</div>
+	);
+}

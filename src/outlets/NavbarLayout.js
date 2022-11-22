329bcdf06b725860713c58f9a5ba61ffe3e2baad
JiokKae <ekaktm1@gmail.com>
2022-11-22 09:04:14 +0900

    Create GlobalStyle in NavbarLayout

--- src/outlets/NavbarLayout.js
+++ src/outlets/NavbarLayout.js
@@ -1,13 +1,23 @@
 import { Outlet } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 import Navbar from "../components/Navbar";
+
+const GlobalStyle = createGlobalStyle`
+	@media (max-width: 576px)
+	{
+		html { font-size: .75rem; }
+	}
+	body { background-color: #f5f5f5; }
+`;
 
 export default function NavbarLayout() {
 	return (
-		<div>
+		<>
+			<GlobalStyle />
 			<Navbar />
 			<div className="container">
 				<Outlet />
 			</div>
-		</div>
+		</>
 	);
 }

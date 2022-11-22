5e9160e9804d01c2a55e036fa8fee10e7221ea70
JiokKae <ekaktm1@gmail.com>
2022-11-22 09:05:24 +0900

    Update Home Route

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,16 +3,17 @@ import styled from "styled-components";
 import MyCharacters from "../components/MyCharacters";
 import { ME } from "../constants/querys";
 
-const Body = styled.div`
+const Container = styled.div`
 	max-width: 780px;
 	margin: auto;
 `;
 
 export default function Home() {
-	const { data } = useQuery(ME);
+	const { data, loading } = useQuery(ME);
+
 	return (
-		<Body>
-			{data?.me ? <MyCharacters /> : null}
-		</Body>
+		<Container>
+			{loading ? null : data.me ? <MyCharacters /> : null}
+		</Container>
 	);
 }

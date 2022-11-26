304c8d766581aee4564043e5e12d76b45dc9c25d
JiokKae <ekaktm1@gmail.com>
2022-11-26 09:01:38 +0900

    내 캐릭터 닉네임을 등록하지 않게 검사

--- src/components/RegisterProfile.js
+++ src/components/RegisterProfile.js
@@ -1,8 +1,12 @@
-import { useMutation } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import { useState } from "react";
 import { ME, REGISTER_PROFILE } from "../constants/querys";
 
 export default function RegisterProfile() {
+	const {
+		data: { me = null },
+		loading,
+	} = useQuery(ME);
 	const [characterName, setCharacterName] = useState("");
 	const [registerProfile] = useMutation(REGISTER_PROFILE, {
 		onCompleted: ({ registerProfile }) => {
@@ -15,11 +19,22 @@ export default function RegisterProfile() {
 		refetchQueries: [{ query: ME }],
 	});
 
+	const isMyCharacter = (characterName) => {
+		if (loading === true || me === null) return false;
+		return me.profiles.some(
+			(profile) => profile.character.name === characterName
+		);
+	};
+
 	const onSubmit = (e) => {
 		e.preventDefault();
 		var name = characterName.trim();
 		if (name.length === 0) {
 			alert("등록할 캐릭터 이름을 입력하세요");
+			return;
+		}
+		if (isMyCharacter(name) === true) {
+			alert("이미 등록한 캐릭터입니다");
 			return;
 		}
 		registerProfile({ variables: { input: { characterName: name } } });
